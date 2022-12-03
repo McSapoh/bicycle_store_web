057fc2b1c6286fcc07642fe19f5c0f03099e1f32
McSapoh <bodyamelnyk04@gmail.com>
2022-12-03 21:56:35 +0200

    4

---
+++ bicycle_store_web/Interfaces/ITypeRepository1.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace bicycle_store_web.Interfaces
+{
+    public interface ITypeRepository : IGenericRepository<Bicycle>
+    {
+        public SelectList GetSelectList();
+    }
+}
